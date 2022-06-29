d915f4deea53752078713fbd71746302a0fba290
Vernitra Jones <vernitrasmedia@gmail.com>
2022-06-29 11:11:39 -0500

    commit tree 926de81cd86cad077943c90c2c9c9f5b0b39f85b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+/*document.body.append(newHeader);*/
+newHeader.id = 'victory';
+newHeader.textContent = "Vernitra is the champion";
